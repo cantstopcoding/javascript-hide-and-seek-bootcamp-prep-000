4402f681760210e8158d5ca715044dbc97e17141
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-20 23:28:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
-
-function nestedTarget() {
-  return document.querySelector('div#nested div div div div');
-}
-
-function increaseRankBy(n) {
-  let lis = document.querySelectorAll('.ranked-list');
-  
-  for(let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i]) + n ;
-  } 
-}
-
-function deepestChild() {
-  let node = document.getElementById('grand-node');
-  let nextNode = node.children[0];
-
-  while (nextNode) {
-    node = nextNode;
-    nextNode = node.children[0];
-  }
-
-  return node;
-}
-
-/*
-describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
-      expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
-    })
-  })
-*/
