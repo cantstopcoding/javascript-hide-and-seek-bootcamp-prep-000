5f7f1c4fb3a0ff1e1787a5723ed281bee38f55ae
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-21 15:01:16 +0000

    Done.

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ function nestedTarget() {
 
 function deepestChild() {
   let node = document.getElementById('grand-node');
-  let nextNode = node.children[0];
+  let next = node.children[0];
   
-  while(nextNode) {
-    node = nextNode;
-    nextNode = node.children[0];
+  while(next) {
+    node = next;
+    next = node.children[0];
   }
-  
   return node;
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li');
+  
+  for(let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
+  }
+  /* var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    for (var i=0;i<lis.length;i++) {
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    } */
 }
