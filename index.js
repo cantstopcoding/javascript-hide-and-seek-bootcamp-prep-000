35132fe8583fe7ef304b87609c678df9b5bf444a
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-21 15:01:34 +0000

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,5 @@ function increaseRankBy(n) {
   for(let i = 0; i < lis.length; i++) {
    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
   }
-  /* var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-    for (var i=0;i<lis.length;i++) {
-      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
-    } */
+  
 }
