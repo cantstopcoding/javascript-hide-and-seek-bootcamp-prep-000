cd0b5f1de1db3f801ef86f167d241de440e056bb
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-26 23:55:00 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   let node = document.getElementById('grand-node');
   let next = node.children[0];
-
+  
   while(next) {
     node = next;
     next = node.children[0];
