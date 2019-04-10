9292d3eebef66a5a5fd7ed6a06b1118bc05d8f28
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-10 01:31:48 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  let node = document.querySelector('#grand-node');
+  let next = node.children[0];
+  
+  while(next) {
+    node = next;
+    next = node.children[0];
+  }
+  return node;
+}
+
+function increaseRankBy(n) {
+  let rankedLists = document.querySelectorAll('.ranked-list');
+  
+  for(let i = 0; i < rankedLists.length; i++) {
+    let children = rankedLists[i].children;
+    
+    for(let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+
