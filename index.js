809c7677b07c39c3c8584eee670f7f8ad658968e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-13 02:23:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function deepestChild() {
   let node = document.getElementById('grand-node');
   let next = node.children[0];
 
-  while (nextNode) {
+  while (next) {
     node = next;
     next = node.children[0];
   }
