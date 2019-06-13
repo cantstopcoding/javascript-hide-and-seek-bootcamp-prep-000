95413ca96b63ee0e6df0b087bbbba6f8786afb9c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-13 02:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  let node = document.getElementByID('grand-node');
-  let next = node.children[0]
-  while(next) {
-    node = next;
-    next = node.children[0];
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
   }
+
   return node;
 }
