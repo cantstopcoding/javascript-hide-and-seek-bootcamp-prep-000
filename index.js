c03e06fbcebbafb9b790cedee86d34bf343b4c9e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-13 02:14:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
 
-
+function deepestChild() {
+  let child = document.querySelector('div#grand-node');
+  let next = '';
+  while(child) {
+    
+  }
+}
