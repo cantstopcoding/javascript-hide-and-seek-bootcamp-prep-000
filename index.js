9d8420802c4b0fd6a041e6461553792145e6365e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-13 02:23:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function nestedTarget() {
 
 function deepestChild() {
   let node = document.getElementById('grand-node');
-  let nextNode = node.children[0];
+  let next = node.children[0];
 
   while (nextNode) {
-    node = nextNode;
-    nextNode = node.children[0];
+    node = next;
+    next = node.children[0];
   }
 
   return node;
